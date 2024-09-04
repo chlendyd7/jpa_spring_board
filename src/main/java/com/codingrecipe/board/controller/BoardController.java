12f58490195617c937144c0d5c426a19345865f1
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-09-04 23:16:29 +0900

    save page DTO init

---
+++ src/main/java/com/codingrecipe/board/controller/BoardController.java
@@ -0,0 +1,23 @@
+package com.codingrecipe.board.controller;
+
+import com.codingrecipe.board.controller.dto.BoardDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+    @GetMapping("/save")
+    public String saveForm() {
+        return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute BoardDTO boardDTO) {
+        System.out.println("boardDTO = " + boardDTO);
+        return null;
+    }
+}
